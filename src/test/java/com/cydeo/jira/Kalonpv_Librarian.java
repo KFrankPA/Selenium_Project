6daea5f5054fa2910c916c5b86dff56693a24ade
KFrankPA <114306682+KFrankPA@users.noreply.github.com>
2023-02-20 12:51:03 -0500

    2/20/2023

---
+++ src/test/java/com/cydeo/jira/Kalonpv_Librarian.java
@@ -0,0 +1,56 @@
+package com.cydeo.jira;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+public class Kalonpv_Librarian {
+    //As a librarian, I should be able to edit the books' information.
+    //
+    //1 Verify the librarian can edit any book info from the books page.
+    //2 There should be a pop up window displayed when user clicks the "Edit Book* button
+    //3 "Edit Book Information" should be displayed on top of the pop up window.
+    //4 "The book has been updated" confirmation message should be displayed.
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+    @Test
+
+    public void AC1_Edit_Button_IsDisplayed(){
+        driver.get(ConfigurationReader.getProperty("env"));
+        BrowserUtils.sleep(2);
+
+        WebElement loginBox = driver.findElement(By.xpath("//input[@id='inputEmail']"));
+        loginBox.sendKeys(ConfigurationReader.getProperty("username"));
+
+        BrowserUtils.sleep(3);
+
+        WebElement passwordBox = driver.findElement(By.xpath("//input[@id='inputPassword']"));
+        passwordBox.sendKeys(ConfigurationReader.getProperty("password"));
+
+        BrowserUtils.sleep(2);
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        driver.findElement(By.xpath("//a[@href='#books']//span[1]")).click();
+
+       // Assert.assertTrue(driver.findElement(By.xpath("//a[contains(@onclick, 'Books.edit_book')]")).isDisplayed());
+
+    }
+
+}
+
+
+
