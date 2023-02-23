526c019a66880b6fd4d26d286858abf984b4c097
KFrankPA <114306682+KFrankPA@users.noreply.github.com>
2023-02-23 15:36:35 -0500

    2/23/2023

--- src/test/java/com/cydeo/jira/Kalonov_Librarian.java
+++ src/test/java/com/cydeo/jira/Kalonov_Librarian.java
@@ -4,6 +4,7 @@ import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -19,28 +20,30 @@ public class Kalonov_Librarian {
     //3 "Edit Book Information" should be displayed on top of the pop up window.
     //4 "The book has been updated" confirmation message should be displayed.
     WebDriver driver;
+
     @BeforeMethod
-    public void setupMethod(){
-        driver= WebDriverFactory.getDriver("chrome");
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
+
     @Test
 
-    public void AC1_Edit_Button_IsDisplayed(){
+    public void AC1_Edit_Button_IsDisplayed() {
         driver.get(ConfigurationReader.getProperty("website"));
-        BrowserUtils.sleep(2);
+        BrowserUtils.sleep(1);
 
         WebElement loginBox = driver.findElement(By.xpath("//input[@id='inputEmail']"));
         loginBox.sendKeys(ConfigurationReader.getProperty("username"));
 
-        BrowserUtils.sleep(3);
+        BrowserUtils.sleep(1);
 
         WebElement passwordBox = driver.findElement(By.xpath("//input[@id='inputPassword']"));
         passwordBox.sendKeys(ConfigurationReader.getProperty("password"));
 
-        BrowserUtils.sleep(2);
+        BrowserUtils.sleep(1);
 
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
@@ -51,22 +54,25 @@ public class Kalonov_Librarian {
 
         driver.findElement(By.xpath("//a[contains(@onclick, 'Books.edit_book')]")).click();
 
-        BrowserUtils.sleep(3);
+        BrowserUtils.sleep(2);
         driver.findElement(By.xpath("//input[@name='name']")).clear();
 
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys("Hello Team Nova");
 
-        BrowserUtils.sleep(5);
 
         Assert.assertTrue(driver.findElement(By.xpath("//button[@type='submit']")).isDisplayed());
 
         driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-
+        ((JavascriptExecutor) driver).executeScript("window-open('https://google.com')");
 
     }
 
-}
+    @Test
+    public void test2() {
+        driver.get("https://library2.cydeo.com/#books");
 
+    }
+}
 
 
